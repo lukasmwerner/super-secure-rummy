16edf8af3ace5c385452bf44d64bb8e103da9b3d
Grant Lemons <grantlemons@aol.com>
2026-04-04 00:27:25 -0600

    add server creation outline to main

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"net"
+
+	_ "charm.land/bubbletea/v2"
+	"charm.land/log/v2"
+	"charm.land/wish/v2"
+	"charm.land/wish/v2/activeterm"
+	"charm.land/wish/v2/bubbletea"
+	"charm.land/wish/v2/logging"
+	"github.com/charmbracelet/ssh"
+)
+
+const (
+	host = "localhost"
+	port = "23234"
+)
 
 func main() {
-	fmt.Println("K ❤️, J ♦︎")
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		ssh.AllocatePty(),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(),
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
 }
